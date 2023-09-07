8419524110f8326eb9378348017668bccb2f3001
Timothy Severson <v4sh101@gmail.com>
2023-09-07 15:06:32 -0600

    Use IIS

--- src/CureSort2/Program.cs
+++ src/CureSort2/Program.cs
@@ -18,6 +18,6 @@ namespace CureSort2
 
         public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
             WebHost.CreateDefaultBuilder(args)
-                .UseStartup<Startup>();
+                .UseStartup<Startup>().UseIIS();
     }
 }

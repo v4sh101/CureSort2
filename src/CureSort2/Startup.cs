e324ea23ce41f289e4f09e8cd20fed661abe3c0b
Timothy Severson <v4sh101@gmail.com>
2023-09-07 15:04:10 -0600

    Apply Changes to .NET Version

--- src/CureSort2/Startup.cs
+++ src/CureSort2/Startup.cs
@@ -56,7 +56,7 @@ namespace CureSort2
             });
 
             services.AddMvc()
-                .SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+                .SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
 
             // Add application services.
             services.AddTransient<IEmailSender, AuthMessageSender>();
@@ -67,9 +67,6 @@ namespace CureSort2
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory, CureContext context)
         {
-            loggerFactory.AddConsole(Configuration.GetSection("Logging"));
-            loggerFactory.AddDebug();
-
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
